da6004f9d83a334e68964a115611e53c0cd46443
Kazuho Okui <kazuho@gmail.com>
2016-06-08 14:27:28 -0700

    Added empty unit test

---
+++ RK4SpringDemoTests/RK4SpringDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  RK4SpringDemoTests.swift
+//  RK4SpringDemoTests
+//
+//  Created by Kazuho Okui on 6/8/16.
+//  Copyright © 2016 Flinto. All rights reserved.
+//
+
+import XCTest
+
+class RK4SpringDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+      XCTAssert(false)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
