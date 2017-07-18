7707affee0bd69a7738a540527749e9b28b4d640
Kazuho Okui <kazuho@gmail.com>
2017-07-18 12:22:36 -0700

    Fix iOS public header name

---
+++ RK4Spring_iOS/RK4Spring.h
@@ -0,0 +1,19 @@
+//
+//  RK4Spring.h
+//  RK4Spring
+//
+//  Created by Kazuho Okui on 7/18/17.
+//  Copyright © 2017 Flinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RK4Spring.
+FOUNDATION_EXPORT double RK4SpringVersionNumber;
+
+//! Project version string for RK4Spring.
+FOUNDATION_EXPORT const unsigned char RK4SpringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RK4Spring/PublicHeader.h>
+
+
